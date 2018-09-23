f4613c1c75c9b55bcffdd5241612597b09665bdf
Philip Potter <philip.g.potter@gmail.com>
2018-09-23 21:02:07 +0100

    replace evilvte with sakura

--- configuration.nix
+++ configuration.nix
@@ -41,9 +41,9 @@
   ''; };
 
   environment.systemPackages = with pkgs; [
-    chromium curl darktable emacs evilvte firefox git gnupg
-    openjdk8 owncloudclient pass skype wget yubikey-personalization
+    chromium curl darktable emacs firefox git gnupg
     go htop libgphoto2
+    openjdk8 owncloudclient pass sakura skype wget yubikey-personalization
   ];
 
   programs.bash.enableCompletion = true;
@@ -56,48 +56,6 @@
   # automatically optimise new files as they are added to the store
   # saves lots of disk space!
   nix.extraOptions = "auto-optimise-store = true";
-
-  nixpkgs.config.evilvte.config = ''
-    #define BACKGROUND_SATURATION 0.15
-    #define BACKGROUND_OPACITY TRUE
-    #define COLOR_STYLE ZENBURN_DARK
-    #define COMMAND_AT_ROOT_WINDOW TRUE
-    #define COMMAND_DOCK_MODE TRUE
-    #define COMMAND_EXEC_PROGRAM TRUE
-    #define COMMAND_FULLSCREEN TRUE
-    #define COMMAND_FONT TRUE
-    #define COMMAND_GEOMETRY TRUE
-    #define COMMAND_LOGIN_SHELL TRUE
-    #define COMMAND_SET_TITLE TRUE
-    #define COMMAND_SHOW_HELP TRUE
-    #define COMMAND_SHOW_OPTIONS TRUE
-    #define COMMAND_SHOW_VERSION TRUE
-    #define COMMAND_TAB_NUMBERS TRUE
-    /* #define FONT "Pragmata Pro 15" */
-    #define FONT_ANTI_ALIAS TRUE
-    #define FONT_ENABLE_BOLD_TEXT TRUE
-    #define MOUSE_CTRL_SATURATION TRUE
-    #define SCROLL_LINES 100000
-    #define SCROLLBAR OFF_R
-    #define SHOW_WINDOW_BORDER FALSE
-    #define SHOW_WINDOW_DECORATED TRUE
-    #define SHOW_WINDOW_ICON TRUE
-    #define WORD_CHARS "-A-Za-z0-9_$.+!*(),;:@&=?/~#%"
-    #define TAB TRUE
-    #define TAB_NEW_PATH_EQUAL_OLD TRUE
-    #define TAB_SHOW_INFO_AT_TITLE TRUE
-    #define TABBAR FALSE
-    #define HOTKEY TRUE
-    #define HOTKEY_COPY ALT(GDK_C) || ALT(GDK_c)
-    #define HOTKEY_PASTE ALT(GDK_V) || ALT(GDK_v)
-    #define HOTKEY_FONT_BIGGER ALT(GDK_plus)
-    #define HOTKEY_FONT_SMALLER ALT(GDK_minus)
-    #define HOTKEY_FONT_DEFAULT_SIZE ALT(GDK_0)
-    #define HOTKEY_TAB_ADD CTRL_SHIFT(GDK_T) || CTRL_SHIFT(GDK_t)
-    #define HOTKEY_TAB_REMOVE CTRL_SHIFT(GDK_W) || CTRL_SHIFT(GDK_w)
-    #define HOTKEY_TAB_PREVIOUS CTRL_ALT(GDK_Page_Up)
-    #define HOTKEY_TAB_NEXT CTRL_ALT(GDK_Page_Down)
-  '';
 
   services.pcscd.enable = true; # needed for yubikey OpenPGP
 
